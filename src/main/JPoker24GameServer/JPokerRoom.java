af09c6e536caca87693a0c7e5edc6dfbcb396701
Adwithya <avmagow@gmail.com>
2021-05-09 22:52:06 +0800

    Add JPokerRoom and JPokerRoomManager

---
+++ src/main/JPoker24GameServer/JPokerRoom.java
@@ -0,0 +1,56 @@
+package JPoker24GameServer;
+
+import Common.JMSHelper;
+import Common.JPokerUser;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class JPokerRoom {
+    private final long startTime;
+    private int timeout = 10;
+    private ArrayList<JPokerUser> players = new ArrayList<>(4);
+
+    protected JPokerRoom() {
+        this.startTime = new Date().getTime();
+    }
+
+    protected JPokerRoom(int timeout) {
+        this.startTime = new Date().getTime();
+        this.timeout = timeout;
+    }
+
+    boolean hasPlayer(String username) {
+        for (JPokerUser player : players) {
+            if (player.getName().equals(username))
+                return true;
+        }
+        return false;
+    }
+
+    boolean isFull(){
+        return players.size() >= 4;
+    }
+
+    void addPlayer(JPokerUser player){
+        if(players.size() >= 4){
+            System.err.println("The room already has 4 users");
+            return;
+        }
+        players.add(player);
+    }
+
+    void ready(){
+        while(true){
+            long currTime = new Date().getTime();
+            if(players.size() == 4 || players.size() >= 2 && currTime - startTime > 10){
+                break;
+            }
+        }
+        start();
+    }
+
+    void start() {
+        System.out.println("Game started");
+    }
+}
---
+++ src/main/JPoker24GameServer/JPokerRoomManager.java
@@ -0,0 +1,39 @@
+package JPoker24GameServer;
+
+import Common.JPokerUser;
+
+import java.util.ArrayList;
+
+public class JPokerRoomManager {
+
+    private final ArrayList<JPokerRoom> rooms = new ArrayList<>();
+
+    JPokerRoom findRoomByUsername(String username) {
+        for (JPokerRoom room : rooms) {
+            if (room.hasPlayer(username))
+                return room;
+        }
+        return null;
+    }
+
+    boolean isUserPlaying(String username) {
+        return findRoomByUsername(username) != null;
+    }
+
+    void allocateRoom(JPokerUser user) {
+        synchronized (rooms) {
+            for (JPokerRoom room : rooms) {
+                if (!room.isFull())
+                    room.addPlayer(user);
+            }
+        }
+
+        System.out.println("Room allocated");
+        JPokerRoom room = new JPokerRoom();
+        room.addPlayer(user);
+        synchronized (rooms) {
+            rooms.add(room);
+        }
+        new Thread(room::ready).start();
+    }
+}
